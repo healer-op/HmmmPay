954715bfa3dac2a9fdeb8520c16b39b86e0cf38a
HEALER <65026164+healer-op@users.noreply.github.com>
2024-08-21 22:19:14 +0530

    Add files via upload

--- src/js/hmmm.js
+++ src/js/hmmm.js
@@ -6,10 +6,14 @@ var message = params.get("message");
 var upi_id = params.get("upi");
 var upilink = `upi://pay?pa=${upi_id}&pn=HmmmPay!&tn=${message}&am=${amount}`
 
+if(!amount){
+
+}else{
+
 var qrlink = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(upilink)}&size=400x400&color=105-255-107&bgcolor=1a1a1a&format`
 document.getElementById("qr").src = qrlink
 document.getElementById("details").innerHTML = `Scan with any upi app<br><b>${upi_id}</b> has requested you and payment of <b>₹${amount}</b> for <b>${message}</b>`
-
+}
 
 function upiOpen(){
     UpiSound.play()
