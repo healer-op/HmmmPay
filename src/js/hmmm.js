d3027caeeb23b4ea861e680510f770a982b0ebc8
HEALER <65026164+healer-op@users.noreply.github.com>
2024-08-22 13:17:54 +0530

    ⚡

--- src/js/hmmm.js
+++ src/js/hmmm.js
@@ -7,17 +7,23 @@ var upi_id = params.get("upi");
 var upilink = `upi://pay?pa=${upi_id}&pn=HmmmPay!&tn=${message}&am=${amount}`
 
 if(!amount){
-
+    document.getElementById("button").innerText = "Copy Url"
 }else{
 
 var qrlink = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(upilink)}&size=400x400&color=105-255-107&bgcolor=1a1a1a&format`
 document.getElementById("qr").src = qrlink
-document.getElementById("details").innerHTML = `Scan with any upi app<br><b>${upi_id}</b> has requested you and payment of <b>₹${amount}</b> for <b>${message}</b>`
+document.getElementById("details").innerHTML = `Scan with any upi app<br><span id="url" style="padding: 0em 1.2em;"><b>${upi_id}</b> has requested you and payment of <b>₹${amount}</b> for <b>${message}</b></span>`
 }
 
 function upiOpen(){
     UpiSound.play()
-    window.open(upilink, "_self");
+    if(!amount){
+       navigator.clipboard.writeText(document.getElementById("url").innerText);
+       document.getElementById("button").innerText = "Copied to Clipboard"
+    }else{
+        window.open(upilink, "_self");
+    }
+    
 }
 
 function github(){
