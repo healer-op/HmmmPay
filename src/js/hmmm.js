1c142b0f6fa742fbed95fca9ffe428f37c78ffcb
HEALER <65026164+healer-op@users.noreply.github.com>
2024-08-24 17:03:58 +0530

    Update hmmm.js

--- src/js/hmmm.js
+++ src/js/hmmm.js
@@ -6,27 +6,30 @@ var message = params.get("message");
 var upi_id = params.get("upi");
 var upilink = `upi://pay?pn=via%20hmmmPay&pa=${upi_id}&cu=INR&am=${amount}`
 
-if(!amount){
+if (!amount) {
     document.getElementById("button").innerText = "Copy Url"
-}else{
+} else {
 
-var qrlink = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(upilink)}&size=400x400&color=105-255-107&bgcolor=1a1a1a`
-document.getElementById("qr").src = qrlink
-document.getElementById("details").innerHTML = `Scan with any upi app<br><span id="url" style="padding: 0em 1.2em;"><b>${upi_id}</b> has requested you and payment of <b>₹${amount}</b> for <b>${message}</b></span>`
+    var qrlink = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(upilink)}&size=400x400&color=105-255-107&bgcolor=1a1a1a`
+    document.getElementById("qr").src = qrlink
+    document.getElementById("details").innerHTML = `Scan with any upi app<br><span id="url" style="padding: 0em 1.2em;"><b>${upi_id}</b> has requested you and payment of <b>₹${amount}</b> for <b>${message}</b></span>`
 }
 
-function upiOpen(){
+function upiOpen() {
     UpiSound.play()
-    if(!amount){
-       navigator.clipboard.writeText((document.getElementById("url").innerText).replace("you can click to edit",""));
-       document.getElementById("button").innerText = "Copied to Clipboard"
-    }else{
+    if (!amount) {
+        var link = (document.getElementById("url").innerText).replace("you can click to edit", "");
+        var k = encodeURIComponent(link.split("message=")[1])
+        var l = link.split("message=")[0] + "message=" + k;
+        navigator.clipboard.writeText(l);
+        document.getElementById("button").innerText = "Copied to Clipboard"
+    } else {
         window.open(upilink, "_self");
     }
-    
+
 }
 
-function github(){
+function github() {
     window.open("https://github.com/healer-op");
 }
 //?upi=sigmachad@ibl&amount=30.00&message=HmmmmPay
